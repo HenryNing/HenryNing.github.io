28a2ffafdb9184a2c532445249b00d922ac99841
HenryNing <46895466+HenryNing@users.noreply.github.com>
2019-02-17 19:45:59 +0800

    Update _alert.js

--- _alert.js
+++ _alert.js
@@ -6,31 +6,10 @@ function alert(data, callback) {
 	alert_btn = document.createElement('div'),
 	textNode = document.createTextNode(data ? data : ''),
 	btnText = document.createTextNode('确 定');
-	css(alert_bg, {
-'position': 'fixed','top': '0','left': '0','right': '0','bottom': '0','background-color': 'rgba(0, 0, 0, 0.1)','z-index': '999999999'
-	});
-	css(alert_box, {
-'width': '270px',
-'max-width': '90%',
-'font-size': '16px',
-'text-align': 'center',
-'background-color': '#fff',
-'border-radius': '15px',
-'position': 'absolute',
-'top': '50%',
-'left': '50%',
-'transform': 'translate(-50%, -50%)'
-	});
-	css(alert_text, {
-'padding': '10px 15px',
-'border-bottom': '1px solid #ddd'
-	});
-	css(alert_btn, {
-'padding': '10px 0',
-'color': '#bb00bb',
-'font-weight': '600',
-'cursor': 'pointer'
-	});
+	css(alert_bg, {'position': 'fixed','top': '0','left': '0','right': '0','bottom': '0','background-color': 'rgba(0, 0, 0, 0.1)','z-index': '999999999'});
+	css(alert_box, {'width': '270px','max-width': '90%','font-size': '16px','text-align': 'center','background-color': '#fff','border-radius': '15px','position': 'absolute','top': '50%','left': '50%','transform': 'translate(-50%, -50%)'});
+	css(alert_text, {'padding': '10px 15px','border-bottom': '1px solid #ddd'});
+	css(alert_btn, {'padding': '10px 0','color': '#bb00bb','font-weight': '600','cursor': 'pointer'});
 	alert_text.appendChild(textNode);
 	alert_btn.appendChild(btnText);
 	alert_box.appendChild(alert_text);
@@ -39,16 +18,13 @@ function alert(data, callback) {
 	document.getElementsByTagName('body')[0].appendChild(alert_bg);
 	alert_btn.onclick = function() {
 		alert_bg.parentNode.removeChild(alert_bg);
-		if (typeof callback === 'function') {
+		if (typeof callback === 'function')
 			callback();
-		}
 	}
 }
-
 function css(targetObj, cssObj) {
 	var str = targetObj.getAttribute("style") ? targetObj.getAttribute('style') : '';
-	for (var i in cssObj) {
+	for (var i in cssObj)
 		str += i + ':' + cssObj[i] + ';';
-	}
 	targetObj.style.cssText = str;
 }
