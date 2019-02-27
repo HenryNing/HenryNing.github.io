8567a2ffc714c944a5952a217690afb9e613d6c2
HenryNing <46895466+HenryNing@users.noreply.github.com>
2019-02-27 23:10:20 +0800

    Update _alert.js

--- _alert.js
+++ _alert.js
@@ -1,30 +1,19 @@
-function _alert(data, callback) {
-	var alert_bg = document.createElement('div');
-	alert_box = document.createElement('div'),
-	alert_text = document.createElement('div'),
-	alert_btn = document.createElement('div'),
-	textNode = document.createTextNode(data ? data : ''),
-	btnText = document.createTextNode('确 定');
-	css(alert_bg, {'position': 'fixed','top': '0','left': '0','right': '0','bottom': '0','background-color': 'rgba(0, 0, 0, 0.1)','z-index': '999999999'});
-	css(alert_box, {'width': '270px','max-width': '90%','font-size': '16px','text-align': 'center','background-color': '#fff','border-radius': '15px','position': 'absolute','top': '50%','left': '50%','transform': 'translate(-50%, -50%)'});
-	css(alert_text, {'padding': '10px 15px','border-bottom': '1px solid #ddd'});
-	css(alert_btn, {'padding': '10px 0','color': '#bb00bb','font-weight': '600','cursor': 'pointer'});
-	alert_text.appendChild(textNode);
-	alert_btn.appendChild(btnText);
-	alert_box.appendChild(alert_text);
-	alert_box.appendChild(alert_btn);
-	alert_bg.appendChild(alert_box);
-	document.getElementsByTagName('body')[0].appendChild(alert_bg);
-	alert_btn.onclick = function() {
-		alert_bg.parentNode.removeChild(alert_bg);
-		if (typeof callback === 'function')
-			callback();
+window.alert=alert;
+function alert(data) {
+	var a=document.createElement("div"),p=document.createElement("p"),btn=document.createElement("div"),textNode=document.createTextNode(data?data:""),btnText=document.createTextNode("确 定");
+	css(a,{"position":"fixed","left":"0","right":"0","top":"20%","width":"100px","margin":"0 auto","background-color":"#f00","font-size":"20px","text-align":"center"});
+	css(btn,{"background":"blue"});
+	p.appendChild(textNode);
+	btn.appendChild(btnText);
+	a.appendChild(p),a.appendChild(btn);
+	document.getElementsByTagName("body")[0].appendChild(a);
+	btn.onclick=function {
+		a.parentNode.removeChild(a);
 	}
 }
-function css(targetObj, cssObj) {
-	var tg = targetObj.getAttribute("style");
-	var str = tg ? tg : '';
-	for (var i in cssObj)
-		str += i + ':' + cssObj[i] + ';';
-	targetObj.style.cssText = str;
+function css(trt,coj) {
+	var str=trt.getAttribute("style")?trt.getAttribute("style"):"";
+	for(var i in coj)
+		str+=i+":"+coj[i]+";";
+	targetObj.style.cssText=str;
 }
